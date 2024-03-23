10404957e8ac268554ff13d1d58cd1d3d9056302
Thamires Febbo <thamiresfebbo@hotmail.com>
2024-03-22 22:55:30 -0300

    criada pages do game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+//CSS
 import './App.css';
 
+//React
+import { useCallback, useEffect, useState } from "react";
+
+//data
+import { wordsList } from "./data/word";
+
+//components
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList)
+
+  //starts game
+  const startGame = () =>{
+  setGameStage(stages[1].name);
+  };
+
+//process the letter input
+const verifyLetter = () =>{
+  setGameStage(stages[2].name);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {gameStage === "start" && <StartScreen startGame ={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === "end" && <GameOver />}
     </div>
   );
 }
